f94a10284865763bbfdf336293de3698ee8d0265
Chad <Mapletonkid@gmail.com>
2022-01-11 14:06:21 -0700

    Refactor Form1 to MainForm. Added UI controlls to MainForm.

--- eng_mach/Program.cs
+++ eng_mach/Program.cs
@@ -16,7 +16,7 @@ namespace eng_mach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
